8f9454dce4afd685b8927ad632a6c4bf7dbbba41
Svyatoslav <tortik2000.g@gmail.com>
2026-04-21 14:54:36 +0300

    1.0.2
    треугольник победа

--- SLLE/src/Program.cs
+++ SLLE/src/Program.cs
@@ -3,6 +3,7 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using SLLE.util;
 
 namespace SLLE.src
 {
@@ -20,7 +21,6 @@ namespace SLLE.src
         // переменные для рендера
         int vao;
         int shaderProgram;
-
 
         // размеры экрана
         int width, height;
@@ -63,13 +63,42 @@ namespace SLLE.src
                 vertives.Length * sizeof(float), 
                 vertives, BufferUsageHint.StaticDraw);
 
+            // биндим наш шэйдер Vertex Array Object
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(vao, 0);
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0); // удаляем бинд
+            GL.BindVertexArray(0);
+
+            // создаём шейдер
+            shaderProgram = GL.CreateProgram();
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, util.ShaderManager.LoadRaw("Default.vert"));
+            GL.CompileShader(vertexShader);
+
+            int fragmentSahder = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentSahder, util.ShaderManager.LoadRaw("Default.frag"));
+            GL.CompileShader(fragmentSahder);
+
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentSahder);
+
+            GL.LinkProgram(shaderProgram);
+
+            // удаляем шейдеры
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentSahder);
         }
 
         /// <summary> Метод вызываемый при выключении программы </summary>
         protected override void OnUnload()
         {
             base.OnUnload();
+
+            GL.DeleteVertexArray(vao);
+            GL.DeleteProgram(shaderProgram);
         }
 
         /// <summary> Метод вызываемый при рендере </summary>
@@ -78,6 +107,11 @@ namespace SLLE.src
             // чистим окно
             GL.ClearColor(0, 0, 1f, 1f);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // рисуем треугольник
+            GL.UseProgram(shaderProgram);
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
             // обновляем буфер окна
             Context.SwapBuffers();
---
+++ SLLE/util/ShaderManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLLE.util
+{
+    internal class ShaderManager
+    {
+        public static string LoadRaw(string path)
+        {
+            string source = "";
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../Shaders/" + path))
+                {
+                    source = reader.ReadToEnd();
+                }
+
+                return source;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Faided to load shader!\n" + err.Message);
+
+                return "$err";
+            }
+        }
+    }
+}
